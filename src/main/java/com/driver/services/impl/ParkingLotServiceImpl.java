0815c8bff3505e19c55e024abf83940f33d355fa
unknown <vipulsingh.cg@gmail.com>
2023-03-25 15:57:57 +0530

    sol

--- src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
+++ src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
@@ -1,5 +1,7 @@
 package com.driver.services.impl;
 
+import com.driver.model.ParkingLot;
+import com.driver.model.Spot;
 import com.driver.model.SpotType;
 import com.driver.repository.ParkingLotRepository;
 import com.driver.repository.SpotRepository;
@@ -18,26 +20,56 @@ public class ParkingLotServiceImpl implements ParkingLotService {
     SpotRepository spotRepository1;
     @Override
     public ParkingLot addParkingLot(String name, String address) {
-
+        ParkingLot parkingLot = new ParkingLot();
+        parkingLot.setName(name);
+        parkingLot.setAddress(address);
+        return parkingLotRepository1.save(parkingLot);
     }
 
     @Override
     public Spot addSpot(int parkingLotId, Integer numberOfWheels, Integer pricePerHour) {
+        ParkingLot parkingLot = parkingLotRepository1.findById(parkingLotId).get();
+        Spot spot = new Spot();
+        if(numberOfWheels == 2){
+            spot.setSpotType(SpotType.TWO_WHEELER);
+        } else if (numberOfWheels > 2 && numberOfWheels <= 4) {
+            spot.setSpotType(SpotType.FOUR_WHEELER);
+        }else {
+            spot.setSpotType(SpotType.OTHERS);
+        }
+        spot.setPricePerHour(pricePerHour);
+        spot.setOccupied(false);
 
+        spot.setParkingLot(parkingLot);
+
+        return spotRepository1.save(spot);
     }
 
     @Override
     public void deleteSpot(int spotId) {
-
+        Spot spot = spotRepository1.findById(spotId).get();
+        ParkingLot parkingLot = spot.getParkingLot();
+        parkingLot.getSpotList().remove(spot);
+        spotRepository1.deleteById(spotId);
     }
 
     @Override
     public Spot updateSpot(int parkingLotId, int spotId, int pricePerHour) {
-
+        ParkingLot parkingLot = parkingLotRepository1.findById(parkingLotId).get();
+        Spot spot = spotRepository1.findById(spotId).get();
+        spot.setPricePerHour(pricePerHour);
+        spot.setParkingLot(parkingLot);
+        return spotRepository1.save(spot);
+        //#//
     }
 
     @Override
     public void deleteParkingLot(int parkingLotId) {
-
+        ParkingLot parkingLot = parkingLotRepository1.findById(parkingLotId).get();
+        List<Spot> spotList = parkingLot.getSpotList();
+        for (Spot spot: spotList){
+            spotRepository1.delete(spot);
+        }
+        parkingLotRepository1.delete(parkingLot);
     }
 }
--- src/main/java/com/driver/services/impl/PaymentServiceImpl.java
+++ src/main/java/com/driver/services/impl/PaymentServiceImpl.java
@@ -1,6 +1,9 @@
 package com.driver.services.impl;
 
+import com.driver.model.Payment;
 import com.driver.model.PaymentMode;
+import com.driver.model.Reservation;
+import com.driver.model.Spot;
 import com.driver.repository.PaymentRepository;
 import com.driver.repository.ReservationRepository;
 import com.driver.services.PaymentService;
@@ -16,6 +19,28 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public Payment pay(Integer reservationId, int amountSent, String mode) throws Exception {
+        Reservation reservation = reservationRepository2.findById(reservationId).get();
+        Spot spot = reservation.getSpot();
+        int bill = reservation.getNumberOfHours() * spot.getPricePerHour();
+        if(amountSent < bill){
+            throw new Exception("Insufficient Amount");
+        }
+        String modeOfPayment = mode.toUpperCase();
+        PaymentMode paymentMode;
+        try {
+            paymentMode = PaymentMode.valueOf(modeOfPayment);
+        }catch (Exception e){
+            throw new Exception("Payment mode not detected");
+        }
+        // now make payment
+        Payment payment = reservation.getPayment();
+        payment.setPaymentCompleted(true);
+        payment.setPaymentMode(paymentMode);
+        payment.setReservation(reservation);
 
+        //make the spot occupied
+        spot.setOccupied(true);
+
+        return paymentRepository2.save(payment);
     }
 }
